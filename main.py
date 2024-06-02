93b3a697c945bb2b76c732c17efbf697d490ba49
Johannes <johannes.ronning@tietoevry.com>
2024-06-02 11:41:05 +0200

    updated to use supabase

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 from pathlib import Path
 
 import httpx
+import supabase
 
 @dataclasses.dataclass()
 class Message:
@@ -40,14 +41,20 @@ cache_name = datetime.datetime.today().strftime("%d_%m_%y")
 cache_dir = "tmp"
 cache_path = f"{cache_dir}/{cache_name}"
 
-stocks_we_care_about = {
-    "johannes.ronning@outlook.com": ["NAS", "NOM", "NONG"],
-    "matiaskje@gmail.com": ["NAS", "NOM", "KOG"]
-}
+supabase_key = os.environ.get("SUPABASE_KEY") or ""
+supabase_url = os.environ.get("SUPABASE_URL") or ""
+
+client = supabase.create_client(
+    supabase_key=supabase_key,
+    supabase_url=supabase_url
+)
 
 def ensure_state():
     if not all([smtp_password, smtp_username]):
         raise ValueError("missing environment variables for SMPT_USERNAME or SMPT_PASSWORD.")
+
+    if not all([supabase_key, supabase_url]):
+        raise ValueError("missing environment variables for SUPABASE_KEY or SUPABASE_URL.")
 
     cache = Path(cache_path)
     if not cache.exists():
@@ -56,7 +63,7 @@ def ensure_state():
 def load_already_checked_ids():
     with open(cache_path, "r") as cache:
         line = cache.readline()
-        return [int(id) for id in list(filter(lambda x: x != "", line.split(" ")))]
+        return list(filter(lambda x: x != "", line.split(" ")))
 
 def get_daily_messages():
     # Their api is retarded, don't ask me why
@@ -90,11 +97,23 @@ def main():
 
     ids_checked_today = load_already_checked_ids()
 
-    for message in get_daily_messages():
-        for user in stocks_we_care_about.keys():
-            add_id_to_cache(message.id)
+    l = client.from_("emails").select("email, user_id, tickers (ticker_name)").execute()
+    data = l.data
 
-            if message.issuerSign and message.issuerSign in stocks_we_care_about[user] and message.id not in ids_checked_today:
+    messages = get_daily_messages()
+
+    for row in data:
+        user = row['email']
+        user_id = row['user_id']
+        tickers = [r['ticker_name'] for r in row['tickers']]
+
+        if user_id in ids_checked_today:
+            continue
+
+        add_id_to_cache(user_id)
+
+        for message in messages:
+            if message.issuerSign and message.issuerSign in tickers:
                 title, content = get_message_content(message.id)
                 send_email(user, title, content)
 
